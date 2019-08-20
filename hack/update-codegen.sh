a8e3f98e296e4c54063ad65e2ef3ee7cbd84c51b
gaohj <gaohj2015@yeah.net>
2019-08-20 20:21:01 +0800

    add canary

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -26,8 +26,15 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-ge
 #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
 ${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
-  github.com/iyacontrol/shareit/pkg/client github.com/iyacontrol/shareit/pkg/apis \
+  github.com/iyacontrol/shareit/pkg/client/cronhpa github.com/iyacontrol/shareit/pkg/apis \
   cronhpa:v1 \
+  --output-base "$(dirname ${BASH_SOURCE})/../../../.." \
+  --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.go.txt
+
+
+${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
+  github.com/iyacontrol/shareit/pkg/client/canary github.com/iyacontrol/shareit/pkg/apis \
+  canary:v1 \
   --output-base "$(dirname ${BASH_SOURCE})/../../../.." \
   --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.go.txt
 
