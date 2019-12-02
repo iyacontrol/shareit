028d7694350de4e8c66cb6873d38db7f22d4faed
gaohj <gaohj2015@yeah.net>
2019-12-02 16:43:58 +0800

    fix

--- pkg/apis/confighpa/v1beta1/register.go
+++ pkg/apis/confighpa/v1beta1/register.go
@@ -29,8 +29,8 @@ var (
 // Adds the list of known types to Scheme.
 func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
-		&Confighpa{},
-		&ConfighpaList{},
+		&ConfigHpa{},
+		&ConfigHpaList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
