59ce14908dbe87d8f1c3195803bdde9bbaf0249a
gaohj <gaohj2015@yeah.net>
2019-12-25 20:25:10 +0800

    fix bug

--- pkg/apis/confighpa/v1beta1/types.go
+++ pkg/apis/confighpa/v1beta1/types.go
@@ -25,8 +25,8 @@ type ConfigHpaSpec struct {
 	// See the comment about this parameter above
 	ScaleUpLimitFactor float64 `json:"scaleUpLimitFactor,omitempty"`
 	// See the comment about this parameter above
-	ScaleDownLimitFactor float64 `json:"scaleUpLimitFactor,omitempty"`
-	Tolerance           float64 `json:"tolerance,omitempty"`
+	ScaleDownLimitFactor float64 `json:"scaleDownLimitFactor,omitempty"`
+	Tolerance            float64 `json:"tolerance,omitempty"`
 
 	// part of HorizontalPodAutoscalerSpec, see comments in the k8s-1.10.8 repo: staging/src/k8s.io/api/autoscaling/v1/types.go
 	// reference to scaled resource; horizontal pod autoscaler will learn the current resource consumption
